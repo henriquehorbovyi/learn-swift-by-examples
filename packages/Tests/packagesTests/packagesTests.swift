dd3b45c151df5b6558b1897a0044db88172c7a6e
Henrique Horbovyi <carlos.souza@paygo.com.br>
2020-09-03 20:46:42 -0300

    adds variables

--- packages/Tests/packagesTests/packagesTests.swift
+++ packages/Tests/packagesTests/packagesTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import packages
+@testable import Program
 
 final class packagesTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(packages().text, "Hello, World!")
+        XCTAssertEqual(Program().text, "Hello, World!")
     }
 
     static var allTests = [
---
+++ variables/variables.swift
@@ -0,0 +1,14 @@
+// Defines a constant value of type Double
+let PI = 3.14
+
+// Defines a variable value of type Int
+var age = 24
+
+// Defines a variale value of type String
+var name: String = "Henrique" 
+
+print(PI)
+print(age)
+print(name)
+
+
